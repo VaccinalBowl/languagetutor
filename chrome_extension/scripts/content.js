bb23e44894d183ed3b9f780f30606fb74777260d
VaccinalBowl <jcummin@tcd.ie>
2013-12-30 01:15:31 +0100

    Refactored and commented content.js and added the images folder to the root directory moving icon.png to it.

--- chrome_extension/scripts/content.js
+++ chrome_extension/scripts/content.js
@@ -1,3 +1,21 @@
+/* 
+File: content.js
+Author: VaccinalBowl
+
+
+This is a content script. It is injected into every page that is loaded into the browser. 
+It waits for messages from the background page. The receipt of the message from the background script triggers the activation or deactivation of the script. 
+
+When the script is active and the user selects some text this script will detect whether or not text has been selected. It will then go through the selection searching for russian words
+and removing all of the other characters in the selection. The array of words is then send back to the background script. 
+
+If this script is not active normal browser behavior occurs. 
+
+
+*/
+
+
+// getSelectedText() gets all of the selected text
 function getSelectedText() {
     var text = "";
     var selection = window.getSelection();
@@ -8,13 +26,10 @@ function getSelectedText() {
     return text;
 }
 
-function doSomethingWithSelectedText() {
-    var selectedText = getSelectedText();
-    if (selectedText) {
-	process_user_input(selectedText);
-	
-    }
-}
+
+/* clearSelection() when call will remove the selection
+   from the browser window or document */
+ 
 function clearSelection() {
     if (window.getSelection) {
         window.getSelection().removeAllRanges();
@@ -23,13 +38,23 @@ function clearSelection() {
     }
 }
 
+/*
+  process_user_input() is called when there is text in the
+  selection. Firstly it clears the selection from visibility
+  and the converts everything to lower case. It removes 
+  all stress marks from the russian text represented normally by
+  \u0301. Then we split the document on anything that does not
+  represent a cyrillic word. The output of the split document is 
+  filtered of duplicates, blanks and '-' and then forwarded back 
+  to the browser. 
+*/
 function process_user_input(text)
 {
 
     clearSelection();
     text=text.toLowerCase();
     text=text.replace(/\u0301/g,"");
-    var re = /[^\u0301\-йцукенгшщзхъёэждлорпавыфячсмитьбю]+/;
+    var re = /[^\-йцукенгшщзхъёэждлорпавыфячсмитьбю]+/;
     words = text.split(re).filter(function(s, i, a) {
 	return a.indexOf(s) === i && s !== "" & s!=="-";
     });;
@@ -38,24 +63,40 @@ function process_user_input(text)
 
 }
 
+/* check_selection() will check if the
+   selection contains some text and if it has some
+   the text will then be pass on to the processor */
 
-var extensionOn = 0; 
-function process_page()
-{
+function check_selection() {
+    var selectedText = getSelectedText();
+    if (selectedText) {
+	process_user_input(selectedText);
+    }
+}
+
+
+var extensionOn = 0; //When the script is first inserted the extension is in the off state.  
+
+/* toggle_script() is called whenever the script receives
+   the message to toggle from the background page. */
+function toggle_script()
+{           
     if(extensionOn==0)
     {
 	console.log("Addon Activated");
-	document.onmouseup = doSomethingWithSelectedText
+	document.onmouseup = check_selection //assigns the action listener when script activated
     }else{
 	console.log("Addon Deactivated");
-	document.onmouseup = function(){};
+	document.onmouseup = null;
     }
     extensionOn^=1;
 
 }
-chrome.runtime.onMessage.addListener(
+/* When the script is injected we register it to receive
+   messages from the background script */ 
+   chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-	process_page();
+	toggle_script();
     }
 );
 
