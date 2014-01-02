c978f8c09200755126e3a478e7fb65d37afa679d
VaccinalBowl <jcummin@tcd.ie>
2014-01-02 03:22:31 +0100

    Made more changes to the exntension with lots of refactoring

--- chrome_extension/scripts/background.js
+++ chrome_extension/scripts/background.js
@@ -1,29 +1,38 @@
+var known_words=[""];
+var new_words;
 
-function activateContentScript()
-{
-    chrome.tabs.query({active: true, currentWindow: true},function(tabs) {
-	chrome.tabs.sendMessage(tabs[0].id, {}, null);
-    });
-}
-var extensionActive = 0; 
-chrome.browserAction.setBadgeText({text:"off"});
-function  on_extension_click(tab)
-{
-    
-    if(extensionActive==0){
-	console.log("Activating Extension");
-	chrome.browserAction.setBadgeText({text:"on"});
 
-    }else{
-	console.log("Deactivating Extension");
-	chrome.browserAction.setBadgeText({text:"off"});
-    }
-    activateContentScript();
-    extensionActive ^= 1;
+
+function download_languagetutor_words()
+{
+    var lt_request = new XMLHttpRequest();
+    lt_request.onreadystatechange=function(){
+	if(lt_request.readyState==4 && lt_request.status==200){
+	    console.log(lt_request.responseText);
+	}
+    };
+    lt_request.open("GET","http://localhost:3000/words.json",true);
+    lt_request.send();
 }
-chrome.browserAction.onClicked.addListener(on_extension_click);
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-	console.log(request);
-    }
+
+
+
+function process_selected_text(info, tab) 
+{
+    download_languagetutor_words();
+    var text = info.selectionText;
+    text=text.toLowerCase();
+    text=text.replace(/\u0301/g,"");
+    var re = /[^\-йцукенгшщзхъёэждлорпавыфячсмитьбю]+/;
+    new_words = text.split(re).filter(function(s, i, a) {
+	return a.indexOf(s) === i && s !== "" & s!=="-";
+    });;
+    console.log(new_words);
+    if(new_words.length>0)
+	chrome.browserAction.setBadgeText({text:"new*"});
+
+}
+chrome.contextMenus.create(
+    {"title": "Send selected text to language tutor", "contexts":["selection"],"onclick": process_selected_text}
 );
+
--- chrome_extension/scripts/popup.js
+++ chrome_extension/scripts/popup.js
@@ -1,1 +1,15 @@
+function init()
+{
+    chrome.browserAction.setBadgeText({text:""});
+    var bgPage = chrome.extension.getBackgroundPage();
+    var new_words = bgPage.new_words;
+    var content_div = document.getElementById("content");
+    var injected_html = "<table><tr><th>word</th></tr>"; //
+    for(var i = 0; i < new_words.length;i++)
+	injected_html+="<tr><td>"+new_words[i]+"</td></tr>";
+    injected_html+="</table>";
+    console.log(injected_html);
+    content_div.innerHTML = injected_html;
+}
 
+window.onload = init;
